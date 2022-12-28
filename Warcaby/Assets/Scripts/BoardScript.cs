a497312fbc6cd6e4700fab3212c6455fe0ab85f2
kwojtik <kawojtik@gmail.com>
2022-12-28 19:02:09 +0100

    Moving
    Added moving mechanics and rules, some bugs need to be fixed

---
+++ Warcaby/Assets/Scripts/BoardScript.cs
@@ -0,0 +1,208 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScript : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject White, Black;
+
+    private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
+    private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
+
+    private bool isUsWhite;
+    private bool isWhiteTurn;
+
+    private Piece selectedPiece;
+
+    private Vector2 mouseOver;
+    private Vector2 startDrag;
+    private Vector2 endDrag;
+
+    private void Start()
+    {
+        GenerateBoard();
+        isWhiteTurn = true;
+    }
+
+    private void Update()
+    {
+        UpdateMouseOver();
+
+
+        //if it is my turn
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if (selectedPiece != null)
+                UpdatePieceDrag(selectedPiece);
+
+            if (Input.GetMouseButtonDown(0)) 
+                SelectPiece(x, y);
+
+            if (Input.GetMouseButtonUp(0)) 
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+        }
+    }
+
+    private void UpdateMouseOver()
+    {
+        if(!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+
+    }
+
+    private void UpdatePieceDrag(Piece p)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        if (x < 0 || x >= pieces.Length || y < 0 || y >= pieces.Length) return;
+
+        Piece p = pieces[x, y];
+
+        if(p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+        }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        // Multiplayer support
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        // Is the move out of bounds?
+        if(x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length)
+        {
+            if (selectedPiece != null)
+                MovePiece(selectedPiece, x1, y1);
+
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+
+            return;
+        }
+
+        // Is there a selected piece?
+        if(selectedPiece != null)
+        {
+            // it didn't move
+            if(endDrag == startDrag)
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+
+                return;
+            }
+
+            //check if valid
+            if(selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
+            {
+                //Did we kill
+                if(Mathf.Abs(x1-x2) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if(p != null)
+                    { 
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p);
+                    }
+                }
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+        }
+    }
+
+    private void EndTurn()
+    {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+
+        isWhiteTurn = !isWhiteTurn;
+        checkVictory();
+    }
+
+    private void checkVictory()
+    {
+
+    }
+
+    private void GenerateBoard()
+    {
+        // Generate White pieces
+        for(int y = 0; y < 3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for(int x = 0; x < 8; x+=2)
+            {
+                //Generating the piece
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+
+        // Generate Black pieces
+        for (int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for (int x = 0; x < 8; x += 2)
+            {
+                //Generating the piece
+                GeneratePiece((oddRow) ? x : x + 1, y);
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        bool isWhite = (y > 3) ? false : true;
+
+        GameObject go = Instantiate((isWhite) ? White : Black) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = Vector3.right * x + Vector3.forward * y + boardOffset + pieceOffset;
+    }
+}
---
+++ Warcaby/Assets/Scripts/Piece.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public bool isWhite;
+    public bool isQueen;
+
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
+    {
+        if (board[x2, y2] != null)
+            return false;
+
+        int deltaMove = Mathf.Abs(x1 - x2);
+        int deltaMoveY = y2 - y1;
+
+        if (isWhite || isQueen)
+        {
+            if(deltaMove == 1)
+            {
+                if (deltaMoveY == 1)
+                    return true;
+            }
+            else if(deltaMove == 2)
+            {
+                if (deltaMoveY == 2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if(p != null && p.isWhite != isWhite)
+                        return true;
+                }
+            }
+            
+        }
+
+        if (!isWhite)
+        {
+            if (deltaMove == 1)
+            {
+                if (deltaMoveY == -1)
+                    return true;
+            }
+            else if (deltaMove == 2)
+            {
+                if (deltaMoveY == -2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if (p != null && p.isWhite != isWhite)
+                        return true;
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
